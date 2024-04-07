e93097704bbe7800f5cc6e637f477794726ce3c5
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-04-07 22:27:04 +0530

    git OAuth + supabase auth link revised v0

--- components/nav/LoginForm.tsx
+++ components/nav/LoginForm.tsx
@@ -10,9 +10,9 @@ export default function LoginForm() {
 
     const pathname = usePathname()
 
-    const supabase =  createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL! as string,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! as string
+    const supabase = createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     )
 
     const handleLogin = () => {

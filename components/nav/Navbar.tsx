b974cf0de39f991bd96ff2f41d6b3665dff1dff8
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-04-08 21:58:26 +0530

    login - logout intigrated

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import React from 'react'
 import LoginForm from './LoginForm';
 import { useUser } from '@/lib/store/user';
+import Profile from './Profile';
 
 export default function Navbar() {
   const user = useUser((state) => state.user)
@@ -14,7 +15,7 @@ export default function Navbar() {
         <Link href="/" className='text-2xl font-bold'>Bloxify.co</Link>
         <div className='h-[2px] w-0 rounded-sm group-hover:w-full transition-all bg-green-500'></div>
         </div>
-        {user ? <h1>profile</h1> : <LoginForm />}
+        {user ? <Profile /> : <LoginForm />}
     </nav>
   )
 }
